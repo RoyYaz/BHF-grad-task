50ae20ceb192bddc15452dd401e365cd7162c972
YazbeckR <YazbeckR@W7-FX3P55J>
2016-11-10 16:48:00 +0000

    v1 model classes added and some validations

---
+++ BHF-grad-task/Models/Donation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace BHF_grad_task.Models
+{
+    public class Donation
+    {
+        protected int donateID { get; set; }
+
+        
+        [Range(1, 5000)]
+        [DataType(DataType.Currency)]
+        protected decimal money { get; set; }
+
+        [Display(Name = "DateTime")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy H:mm:ss}", ApplyFormatInEditMode = true)]
+        protected DateTime dateTime { get; set; }
+
+        protected string regularity { get; set; }
+
+        public class DonationDBContext : DbContext
+        {
+            public DbSet<Donation> DonationDB { get; set; }
+        }
+    }
+}    
+    
---
+++ BHF-grad-task/Models/User.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace BHF_grad_task.Models
+{
+    public class User
+    {
+        protected string title { get; set; }
+
+        [Required]
+        [StringLength(35)]
+        protected string forename { get; set; }
+
+        [Required]
+        [StringLength(35)]
+        protected string surname { get; set; }
+
+        [EmailAddress]
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [StringLength(255)]
+        protected string email { get; set; }
+
+        [RegularExpression(@"^[a-zA-Z0-9]+$")]
+        [Required]
+        [StringLength(35)]
+        protected string noAddress { get; set; }
+
+        [RegularExpression(@"^[a-zA-Z]+$")]
+        [Required]
+        [StringLength(35)]
+        protected string address { get; set; }
+
+        [Required]
+        [StringLength(8, MinimumLength =6)]
+        protected string postCode { get; set; }
+
+        [MaxLength(12)]
+        protected int telephone { get; set; }
+
+        public class UserDBContext : DbContext
+        {
+            public DbSet<User> UserDB { get; set; }
+        }
+    }
+}
