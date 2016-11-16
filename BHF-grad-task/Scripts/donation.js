dc067e70ab9771ec47d5a7594dc238695194c671
YazbeckR <YazbeckR@W7-FX3P55J>
2016-11-16 15:51:54 +0000

    v8 update on controller, view, js. not working in update database from view but close

---
+++ BHF-grad-task/Scripts/donation.js
@@ -0,0 +1,17 @@
+﻿$(function () {
+    $(".radioButton").on('click', function () {
+        //console.log($(this));
+        var amount = $(this).val();
+        $("#donation_Money").val(amount);
+    });
+    //$(".radioButton").each(function () {
+    //    var $this = $(this);
+    //    $this.click(function (e) {
+    //        // get the value of this radio button
+    //        var value = e.valueOf[value];
+
+    //        // put value in text box of that id
+    //        $("#donation_Money").val(value);
+    //    });        
+    //});
+});
